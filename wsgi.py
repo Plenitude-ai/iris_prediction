8648e949c7e91617f221421616a01b22e7ec5d80
Axel Durand <axeldurandfontanel@hotmail.fr>
2021-07-24 17:40:57 +0200

    Update wsgi.py with a correct import path (src.main)

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 ##importing the app from main file
-from main import app
+from src.main import app
 
 if __name__ == "__main__":
+    print("\n\n\n\n\nON RENTRE DANS LE MAIN DE WSGI\n\n\n\n\n")
     app.run()
