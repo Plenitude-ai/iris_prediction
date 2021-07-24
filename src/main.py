1ca527440bf3fd5999d9f8d6e89823a3761fd9fa
Axel Durand <axeldurandfontanel@hotmail.fr>
2021-07-24 17:29:44 +0200

    commented out the __name__ in main.py

--- src/main.py
+++ src/main.py
@@ -63,7 +63,7 @@ def get_pred_json():
 
 
 
-if __name__== '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# if __name__== '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
 
   
